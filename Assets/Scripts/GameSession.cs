584cd2cc7ce0031dbeb6d4e0dafddcf3e0cc053a
Adèle Revert <a.revert@meetic-corp.com>
2019-10-31 09:21:43 +0100

    block breaker

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    // config params
+    [Range (0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if(gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
